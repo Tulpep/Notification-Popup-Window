0949bcd6fc9894258acd23335e6e6855d35a21b7
Ricardo Polo <ricardo.polo@tulpep.com>
2014-11-10 18:44:29 -0500

    Nuget creation files

---
+++ Nuget/Build.ps1
@@ -0,0 +1,16 @@
+﻿$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+Write-Output "Downloading nuget.exe from nuget.org"
+Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile $root\Nuget.exe
+
+
+$version = [System.Reflection.Assembly]::LoadFile("$root\Tulpep.NotificationWindow\bin\Release\Tulpep.NotificationWindow.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+Write-Host "Setting .nuspec version tag to $versionStr"
+$content = (Get-Content $root\NuGet\specs.nuspec)
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\specs.nuspec
+
+
+Write-Output "Creating Nuget Package"
+& $root\Nuget.exe pack $root\Nuget\specs.nuspec
